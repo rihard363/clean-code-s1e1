231661648349d1ca23fe7d3bf16f7c1bda7bdf34
Aliaksei Savchenko <strag_king2@mail.ru>
2024-01-06 22:04:52 +0300

    refactor: add alt for img index.html and app.js

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn btn-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt= "button delete"
     deleteButtonImg.className=" btn__img"
     deleteButton.appendChild(deleteButtonImg);
 
